8dea290c11d7da1b90f2a07c7cb9d1fd99956f56
fernandeslouro <jd.louro@outlook.pt>
2022-03-09 16:02:27 +0000

    Add simplest app possible, as reference for exercise
    I would like to have an exercise where participants
    assemble the simplest possible XKY app from scratch.
    I'm still not sure how it will work but I am starting by
    having such an app in the examples

---
+++ examples/simplest_app/partition_0/main.c
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief Simplest partition code
+ */
+
+#include <xky.h>
+#include <bare.h>
+#include <xky_printf.h>
+
+void entry_point(void) {
+
+    xky_printf("Hello World!\n");
+}
